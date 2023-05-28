07145b887472428e09d9f4d37fdfadeb039a871d
confidentmeerkat <confidentmeerkat@gmail.com>
2023-05-28 10:36:48 -0500

    create post model

---
+++ app/models/Post.ts
@@ -0,0 +1,43 @@
+import { db } from "../firebase";
+
+export default class Post {
+  private static path = "posts";
+
+  public id?: string;
+  public title: string;
+  public content: string;
+  public author: string;
+  public coAuthors?: string[];
+  public comments?: string[];
+
+  constructor(post: Post) {
+    this.title = post.title;
+    this.content = post.content;
+    this.author = post.author;
+  }
+
+  public static async create(post: Post) {
+    try {
+      const ref = await db.collection(this.path).add(post);
+
+      return ref;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  public static async find() {
+    try {
+      const result: Post[] = [];
+      const snapShot = await db.collection(this.path).get();
+
+      snapShot.forEach((doc) => {
+        result.push(doc.data() as Post);
+      });
+
+      return result;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
