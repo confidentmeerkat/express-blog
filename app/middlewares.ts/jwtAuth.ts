7cf422fb561502900460c25bbb27fd1acd42c132
confidentmeerkat <confidentmeerkat@gmail.com>
2023-05-29 07:13:06 -0500

    config auth middleware

---
+++ app/middlewares.ts/jwtAuth.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Response } from "express";
+import { expressjwt, Request as JWTRequest } from "express-jwt";
+
+const jwtAuth = [
+  expressjwt({ secret: "shhhhhhared-secret", algorithms: ["HS256"] }),
+  function (req: JWTRequest, res: Response, next: NextFunction) {
+    if (!req?.auth?.id) return res.sendStatus(401);
+
+    next();
+  },
+];
+
+export default jwtAuth;
